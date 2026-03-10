c2ee388bd500dff980b624ff457cfe9abccb4e04
draklays666 <sasaykonstantinov5@gmail.com>
2026-03-10 18:18:05 +0300

    add game

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	public static final int SCR_WIDTH = 1280;
+	public static final int SCR_HEIGHT = 720;
+	int birdX = 0;
+	int birdY = 0;
+	int birdSpeed = 5;
+
+	Texture birdTexture;
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		birdTexture = new Texture("bird0.png");
+	}
+
+	@Override
+	public void render () {
+		birdX += birdSpeed;
+		birdY += birdSpeed;
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(birdTexture,birdX, birdY);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		birdTexture.dispose();
+	}
+}
